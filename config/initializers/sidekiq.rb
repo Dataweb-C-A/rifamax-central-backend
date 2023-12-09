994e5a886188636ada0e118c34c23285b6d62e9f
Javier Diaz <javierdiazt406@gmail.com>
2023-12-08 22:43:31 -0400

    Docker and private ip

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -7,11 +7,11 @@ schedule_file = 'config/schedule.yml'
 Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file) if File.exist?(schedule_file) && Sidekiq.server?
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: "redis://localhost:6379/1" }
+  config.redis = { url: "redis://10.116.0.3:6379/1" }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: "redis://localhost:6379/1" }
+  config.redis = { url: "redis://10.116.0.3:6379/1" }
 end
 
 Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
