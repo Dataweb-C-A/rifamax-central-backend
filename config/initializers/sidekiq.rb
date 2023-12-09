b8ea6ee38c7d448efe5d0a42bf44cb70ea795665
Javier Diaz <javierdiazt406@gmail.com>
2023-12-08 22:23:38 -0400

    Sidekiq fix

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -6,6 +6,14 @@ require 'sidekiq/web'
 schedule_file = 'config/schedule.yml'
 Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file) if File.exist?(schedule_file) && Sidekiq.server?
 
+Sidekiq.configure_server do |config|
+  config.redis = { url: "redis://localhost:6379/1" }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: "redis://localhost:6379/1" }
+end
+
 Sidekiq::Web.use(Rack::Auth::Basic) do |user, password|
   Rack::Utils.secure_compare(::Digest::SHA256.hexdigest(user), ::Digest::SHA256.hexdigest('superadmin')) &
     Rack::Utils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest('losvergatarios'))
