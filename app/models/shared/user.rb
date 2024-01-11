5e64fd36e3325259cd28d977c1b853ae12adeec5
Javier Diaz <javierdiazt406@gmail.com>
2024-01-11 17:50:04 -0400

    Adding validations

--- app/models/shared/user.rb
+++ app/models/shared/user.rb
@@ -48,6 +48,35 @@ module Shared
     validates :role,
               presence: true
 
+    validates :dni,
+              uniqueness: {
+                message: 'Ya existe un cliente con este número de cédula'
+              },
+              presence: {
+                message: 'Debe introducir un número de cédula'
+              },
+              length: {
+                minimum: 8,
+                message: 'Debe ser mayor a 8 digitos'
+              },
+              format: {
+                with: /\A[VEJG]-\d{1,8}\z/,
+                message: 'Debe incluir (V J E G)'
+              },
+              if: -> { phone[0..3] == '+58 ' }
+              
+    validates :phone,
+              presence: {
+                message: 'Debe introducir un número de teléfono'
+              },
+              uniqueness: {
+                message: 'Ya existe un cliente con este número de teléfono'
+              },
+              format: {
+                with: /\A\+\d{1,4} \(\d{1,4}\) \d{1,10}-\d{1,10}\z/,
+                message: 'Introduzca un número de teléfono válido en el formato: +prefijo telefónico (codigo de area) tres primeros dígitos - dígitos restantes, por ejemplo: +58 (416) 000-0000'
+              }
+
     validates :email,
               presence: true,
               uniqueness: { case_sensitive: false },
