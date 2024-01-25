d3b23f79f57a0fd9144204144856ebe2f65fa095
Javier Diaz <javierdiazt406@gmail.com>
2024-01-25 14:34:22 -0400

    TestAvailable serivce

--- app/workers/x100/tickets_worker.rb
+++ app/workers/x100/tickets_worker.rb
@@ -6,20 +6,7 @@ module X100
     sidekiq_options queue: :default
 
     def perform(*_args)
-      X100::Ticket
-        .reserved
-        .where('updated_at < ?', 1.minutes.ago)
-        .each do |ticket|
-          if ticket.status == 'reserved'
-            ticket.turn_available!
-            puts("Ticket with id: #{ticket.id} is now available!")
-            ticket.update(x100_client_id: nil)
-          else
-            puts("Ticket with id: #{ticket.id} is sold!")
-          end
-        end
-        @tickets = X100::Ticket.all_sold_tickets
-        ActionCable.server.broadcast('x100_tickets', @tickets)
-      end
+      X100::TestAvailable(2, X100::Ticket.reserved.map(&:position))
+    end
   end
 end
