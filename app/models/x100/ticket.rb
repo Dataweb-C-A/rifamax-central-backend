d8164e9c44c688d78d843a5a50cc6b2cbdd3823c
Javier Diaz <javierdiazt406@gmail.com>
2024-01-25 12:28:03 -0400

    Ticket broadcast server

--- app/models/x100/ticket.rb
+++ app/models/x100/ticket.rb
@@ -33,6 +33,7 @@ module X100
     belongs_to :x100_client, class_name: 'X100::Client', foreign_key: 'x100_client_id', optional: true
 
     after_create :generate_order
+    after_update :broadcast_tickets
 
     aasm column: 'status' do
       state :available, initial: true
@@ -100,6 +101,11 @@ module X100
           ticket.save!
         end
       end
+    end
+
+    def broadcast_tickets
+      @tickets = X100::Ticket.all_Sold_tickets
+      ActionCable.server.broadcast('x100_tickets', @tickets)
     end
 
     def self.generate_order(positions)
--- app/workers/x100/tickets_worker.rb
+++ app/workers/x100/tickets_worker.rb
@@ -10,19 +10,16 @@ module X100
         .reserved
         .where('updated_at < ?', 1.minutes.ago)
         .each do |ticket|
-        if ticket.status == 'reserved'
-          ticket.turn_available!
-          puts("Ticket with id: #{ticket.id} is now available!")
-          ticket.update(x100_client_id: nil)
-
-          @tickets = X100::Ticket.all_sold_tickets
-          ActionCable.server.broadcast('x100_tickets', @tickets)
-        else
-          @tickets = X100::Ticket.all_sold_tickets
-          ActionCable.server.broadcast('x100_tickets', @tickets)
-          puts("Ticket with id: #{ticket.id} is sold!")
+          if ticket.status == 'reserved'
+            ticket.turn_available!
+            puts("Ticket with id: #{ticket.id} is now available!")
+            ticket.update(x100_client_id: nil)
+          else
+            puts("Ticket with id: #{ticket.id} is sold!")
+          end
         end
+        @tickets = X100::Ticket.all_sold_tickets
+        ActionCable.server.broadcast('x100_tickets', @tickets)
       end
-    end
   end
 end
