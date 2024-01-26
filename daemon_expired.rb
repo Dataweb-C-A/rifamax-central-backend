dc304e98a766eca0e91f276c8f8afbbf003dda18
Javier Diaz <javierdiazt406@gmail.com>
2024-01-26 12:10:54 -0400

    Fix daemon

--- daemon_expired.rb
+++ daemon_expired.rb
@@ -4,7 +4,7 @@ $redis.config('SET', 'notify-keyspace-events', 'KEA')
 
 $redis.psubscribe('__keyevent@0__:expired') do |on|
   on.pmessage do |pattern, event, key|
-    ticket_id = Number(key.split('_').last)
+    ticket_id = key.split('_').last
     ticket = X100::Ticket.find(ticket_id).turn_available!
     ticket.save!
 
