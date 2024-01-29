fe450f01f24aca851ac9c3261cbf495c0ad9b6e5
Javier Diaz <javierdiazt406@gmail.com>
2024-01-29 14:26:32 -0400

    Fix raffles_controller.rb

--- app/controllers/x100/raffles_controller.rb
+++ app/controllers/x100/raffles_controller.rb
@@ -27,7 +27,7 @@ module X100
       @x100_raffle.shared_user_id = @current_user.id if @current_user.role == 'Taquilla'
 
       @x100_raffle.prizes = [{ name: create_x100_raffle_params[:prizes], prize_position: 1 }]
-      @x100_raffle.prizes = JSON.parse(create_x100_raffle_params[:combos])
+      @x100_raffle.combos = JSON.parse(create_x100_raffle_params[:combos])
       if @x100_raffle.save
         @raffles = X100::Raffle.current_progress_of_actives
 
