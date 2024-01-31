6dc0aebfb93d5e7196460466c4edb1afcd0ffab7
Javier Diaz <javierdiazt406@gmail.com>
2024-01-30 21:50:43 -0400

    API pagination by pagy/extras/array

--- app/controllers/x100/draws_controller.rb
+++ app/controllers/x100/draws_controller.rb
@@ -1,15 +1,22 @@
+require 'pagy/extras/array'
+
 module X100
   class DrawsController < ApplicationController
     include Pagy::Backend
-    
+
     before_action :authorize_request
 
     def raffle_stats
       @raffles = X100::Raffle.raffles_by_user(@current_user)
-      pagy, @paginated_raffles = pagy(@raffles)
+      @pagy, @paginated_raffles = pagy_array(@raffles, items: params[:items_per_page].to_i || 10, page: params[:current_page].to_i || 1)
       render json: {
         raffles: @paginated_raffles,
-        pagination: pagy_metadata(pagy)
+        metadata: {
+          page: @pagy.page,
+          count: @pagy.count,
+          items: @pagy.items,
+          pages: @pagy.pages
+        }
       }, each_serializer: X100::DrawSerializer, status: :ok
     end
   end
