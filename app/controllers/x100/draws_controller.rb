6cbad6e6f78b0ac174f95ee73fa5d3e79ecf967b
Javier Diaz <javierdiazt406@gmail.com>
2024-01-30 21:43:13 -0400

    Testing serializer

--- app/controllers/x100/draws_controller.rb
+++ app/controllers/x100/draws_controller.rb
@@ -4,7 +4,7 @@ module X100
 
     def raffle_stats
       @raffles = X100::Raffle.raffles_by_user(@current_user)
-      render json: @raffles, status: :ok
+      render json: @raffles, each_serializer: X100::DrawSerializer, status: :ok
     end
   end
 end
