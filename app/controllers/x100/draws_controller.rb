1151fbb31c5d990005cf031b303d461de8fbd10e
Javier Diaz <javierdiazt406@gmail.com>
2024-01-30 21:47:17 -0400

    Add pagy to draws controller

--- app/controllers/x100/draws_controller.rb
+++ app/controllers/x100/draws_controller.rb
@@ -1,10 +1,16 @@
 module X100
   class DrawsController < ApplicationController
+    include Pagy::Backend
+    
     before_action :authorize_request
 
     def raffle_stats
       @raffles = X100::Raffle.raffles_by_user(@current_user)
-      render json: @raffles, each_serializer: X100::DrawSerializer, status: :ok
+      pagy, @paginated_raffles = pagy(@raffles)
+      render json: {
+        raffles: @paginated_raffles,
+        pagination: pagy_metadata(pagy)
+      }, each_serializer: X100::DrawSerializer, status: :ok
     end
   end
 end
