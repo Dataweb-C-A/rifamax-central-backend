08e6d8c1df7bf073486c6cb11a036f58e1f3bb82
Javier Diaz <javierdiazt406@gmail.com>
2024-01-30 21:52:27 -0400

    Fix 0

--- app/controllers/x100/draws_controller.rb
+++ app/controllers/x100/draws_controller.rb
@@ -7,8 +7,11 @@ module X100
     before_action :authorize_request
 
     def raffle_stats
+      items_per_page = params[:items_per_page].to_i.positive? ? params[:items_per_page].to_i : 10
+      current_page = params[:current_page].to_i.positive? ? params[:current_page].to_i : 1
+
       @raffles = X100::Raffle.raffles_by_user(@current_user)
-      @pagy, @paginated_raffles = pagy_array(@raffles, items: params[:items_per_page].to_i || 10, page: params[:current_page].to_i || 1)
+      @pagy, @paginated_raffles = pagy_array(@raffles, items: items_per_page, page: current_page)
       render json: {
         raffles: @paginated_raffles,
         metadata: {
